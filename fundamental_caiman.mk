c3c7e6baf5bc06f46164f79edf5df94dc266734c
FundaAgent <323139829+FundaAgent@users.noreply.github.com>
2026-09-08 19:54:20 +0000

    caiman: report the CP2A.260805.005 stock build fingerprint
    The override still carried the BP4A.260205.002 values inherited from the
    16 tree; point BuildDesc/BuildFingerprint at the stock build the blobs and
    kernel come from (incremental 15828068).
    
    Signed-off-by: FundaAgent <323139829+FundaAgent@users.noreply.github.com>

--- fundamental_caiman.mk
+++ fundamental_caiman.mk
@@ -31,8 +31,8 @@ TARGET_SCREEN_HEIGHT := 2856
 TARGET_SCREEN_WIDTH := 1280
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="caiman-user 16 BP4A.260205.002 14624737 release-keys" \
-    BuildFingerprint=google/caiman/caiman:16/BP4A.260205.002/14624737:user/release-keys \
+    BuildDesc="caiman-user 17 CP2A.260805.005 15828068 release-keys" \
+    BuildFingerprint=google/caiman/caiman:17/CP2A.260805.005/15828068:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
